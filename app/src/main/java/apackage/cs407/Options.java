c83dd30c5dc14738bcccff268c86d26a9798e052
Moria\SlabberZ <djahnke@wisc.edu>
2017-04-14 17:22:02 -0500

    "new activities and working buttons"

---
+++ app/src/main/java/apackage/cs407/Options.java
@@ -0,0 +1,18 @@
+package apackage.cs407;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+
+public class Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+    }
+
+}
