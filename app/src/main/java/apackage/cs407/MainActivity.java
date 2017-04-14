393916c13a8660389a6f740bf6976b9ae9d5ef51
Moria\SlabberZ <djahnke@wisc.edu>
2017-04-14 17:19:48 -0500

    "test commit"

--- app/src/main/java/apackage/cs407/MainActivity.java
+++ app/src/main/java/apackage/cs407/MainActivity.java
@@ -1,7 +1,10 @@
 package apackage.cs407;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- int x = 0;
+
+        Button startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+            }
+        });
+
+        Button optionsButton = (Button)findViewById(R.id.optionsButton);
+
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Options.class));
+            }
+        });
+
+        Button leaderboardButton = (Button)findViewById(R.id.leaderboardButton);
+
+        leaderboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LeaderboardActivity.class));
+            }
+        });
     }
+
+
 }
