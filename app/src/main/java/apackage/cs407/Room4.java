4a8471b31a10f8bff2b59d28c106800cd7db3003
OWNERS\Owner <bdeetz@wisc.edu>
2017-05-01 22:06:27 -0500

    added function in rooms 4 and 5, unable to test fully code because computer sucks

--- app/src/main/java/apackage/cs407/Room4.java
+++ app/src/main/java/apackage/cs407/Room4.java
@@ -1,7 +1,12 @@
 package apackage.cs407;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Room4 extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class Room4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room4);
+        ((GlobalApp) getApplication()).setViewItem(null);
+
+        Button Return = (Button) findViewById(R.id.Return);
+        Return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Item door2 = new Item("Door", 100, "", false, 0, R.mipmap.door, "An unlocked door.");
+                ((GlobalApp) getApplication()).setViewItem(door2);
+                door2.addAction(new Enter(true, new Intent(Room4.this, Room2.class)));
+                startActivity(new Intent(Room4.this, ItemView.class));
+            }});
+
+        ImageButton fridgeButton = (ImageButton) findViewById(R.id.Fridge);
+        fridgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Item Fridge = new Item("Fridge", 100, "", false, 0, R.mipmap.fridge, "A fridge, there`s some meat inside.");
+                ((GlobalApp) getApplication()).setViewItem(Fridge);
+                Fridge.addAction(new Take(new Item("Meat", 5, "Meat", true, 0, R.mipmap.meat, "")));
+                startActivity(new Intent(Room4.this, ItemView.class));
+            }});
+
+        ImageButton tableButton = (ImageButton) findViewById(R.id.Table);
+        tableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Item table = new Item("Table", 100, "", false, 0, R.mipmap.table, "a bare table, better look somewhere else");
+                ((GlobalApp) getApplication()).setViewItem(table);
+                startActivity(new Intent(Room4.this, ItemView.class));
+
+            }});
     }
 }
--- app/src/main/java/apackage/cs407/Room5.java
+++ app/src/main/java/apackage/cs407/Room5.java
@@ -1,7 +1,11 @@
 package apackage.cs407;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Room5 extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class Room5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room5);
+        ((GlobalApp) getApplication()).setViewItem(null);
+
+        Button Return = (Button) findViewById(R.id.Return);
+        Return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Item door2 = new Item("Door", 100, "", false, 0, R.mipmap.door, "An unlocked door.");
+                ((GlobalApp) getApplication()).setViewItem(door2);
+                door2.addAction(new Enter(true, new Intent(Room5.this, Room3.class)));
+                startActivity(new Intent(Room5.this, ItemView.class));
+            }});
+
+        final ImageButton ladderButton = (ImageButton) findViewById(R.id.Ladder);
+        ladderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Item curr = ((GlobalApp) getApplication()).getItem();
+                Item ladder = new Item ("Ladder", 1, "A small ladder", true, 0, R.mipmap.ladder, "");
+                ((GlobalApp) getApplication()).setViewItem(ladder);
+                ladder.addAction(new Take(ladderButton));
+
+                startActivity(new Intent(Room5.this, ItemView.class));
+            }
+        });
     }
 }
